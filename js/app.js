4538e6d6e11676683744ef22b8598c7fa9ad68e1
Sebastiao de Paula <sebastiao.paula@gmail.com>
2025-01-20 12:45:56 -0300

    Adicionao contatr de Locação

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
+let contador=1;
+
 function alterarStatus(gameId) {
     let gameItem = document.getElementById(`game-${gameId}`);
     let imagem = gameItem.querySelector('.dashboard__item__img');
     let button = gameItem.querySelector('.dashboard__item__button');
 
     if (button.textContent === 'Alugar') {
-        // Alterar para "Devolver"
+        if(confirm("Deseja Realmente Alugar")){
+            // Alterar para "Devolver"
         button.textContent = 'Devolver';      
         button.classList.add('dashboard__item__button--return');        
         imagem.classList.add('dashboard__item__img--rented');
+        contador++;
+        }
+        
     } else if (button.textContent === 'Devolver') {
+        if (confirm("Deseja Realmente Devolver")){
         // Alterar para "Alugar"
         button.textContent = 'Alugar';
         button.classList.remove('dashboard__item__button--return');                
         imagem.classList.remove('dashboard__item__img--rented');
+        contador --;
+        }
+        
+    }
+    contadors();
+}
+function contadors(){
+    if (contador==1){
+        alert(`Você Tem  ${contador} Jogo Alugado`);
+    }else if(contador > 1){
+        alert(`Você Tem  ${contador} Jogos Alugado`);
+    }else{
+        alert(`Você Não Tem Jogo Alugado`);
     }
 }
-
